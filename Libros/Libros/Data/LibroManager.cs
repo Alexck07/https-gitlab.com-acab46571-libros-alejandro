4ff3f6629227fba12a4244cc67e0673553cb7cba
Alexcr77 Cab <acab46571@gmail.com>
2020-07-26 23:57:11 -0500

    Agregar archivos de proyecto.

---
+++ Libros/Libros/Data/LibroManager.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libros.Data
+{
+    public class LibroManager
+    {
+        const string url = "http://192.168.43.75:3000/libros/";
+        public async Task<IEnumerable<Libro>> GetAll()
+        {
+            HttpClient client = new HttpClient();
+            string result = await client.GetStringAsync(url);
+            return JsonConvert.DeserializeObject<IEnumerable<Libro>>(result);
+        }
+
+    }
+}
